65d2826a1e94404474ea2a108a94fe9784b38e86
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2025-10-30 22:21:16 +0100

    Init project

---
+++ src/client/kotlin/samy/nekomatweaks/NekomaTweaksDataGenerator.kt
@@ -0,0 +1,9 @@
+package samy.nekomatweaks
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object NekomaTweaksDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
