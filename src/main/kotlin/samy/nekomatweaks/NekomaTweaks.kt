4a0b2b634e2ac4ece3359b2d4490cc255a385dde
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2025-11-01 12:05:04 +0100

    Underwater torch progress

--- src/main/kotlin/samy/nekomatweaks/NekomaTweaks.kt
+++ src/main/kotlin/samy/nekomatweaks/NekomaTweaks.kt
@@ -12,19 +12,27 @@ object NekomaTweaks : ModInitializer {
     private val logger = LoggerFactory.getLogger("nekoma-tweaks")
 
     override fun onInitialize() {
-        val edibleComponent = FoodComponent.Builder().nutrition(4).saturationModifier(0.8f).build()
-
-        DefaultItemComponentEvents.MODIFY.register { context ->
-            context.modify(Items.GLISTERING_MELON_SLICE) { builder ->
-                builder
-                    .add(DataComponentTypes.FOOD, edibleComponent)
-                    .add(DataComponentTypes.CONSUMABLE, ConsumableComponents.FOOD)
-            }
-        }
+        initializeEdibleGlisterinMelonSlice()
 
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
         logger.info("Hello Fabric world! I'm making a mod")
+    }
+
+    fun initializeEdibleGlisterinMelonSlice() {
+        val foodComponent = FoodComponent.Builder().nutrition(4).saturationModifier(0.8f).build()
+
+        DefaultItemComponentEvents.MODIFY.register { context ->
+            context.modify(Items.GLISTERING_MELON_SLICE) { builder ->
+                builder
+                    .add(DataComponentTypes.FOOD, foodComponent)
+                    .add(DataComponentTypes.CONSUMABLE, ConsumableComponents.FOOD)
+            }
+        }
+    }
+
+    fun initializeUnderwaterTorch() {
+
     }
 }
