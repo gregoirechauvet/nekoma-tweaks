310430af9d4bfc9d02366bb5ddfcc6b5ecc3d7f9
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2025-11-01 21:42:23 +0100

    Fix tint on redstone cables

--- src/client/kotlin/samy/nekomatweaks/NekomaTweaksClient.kt
+++ src/client/kotlin/samy/nekomatweaks/NekomaTweaksClient.kt
@@ -2,6 +2,8 @@ package samy.nekomatweaks
 
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
+import net.minecraft.block.RedstoneWireBlock
 import net.minecraft.client.render.BlockRenderLayer
 import org.slf4j.LoggerFactory
 
@@ -10,6 +12,10 @@ object NekomaTweaksClient : ClientModInitializer {
 
     override fun onInitializeClient() {
         BlockRenderLayerMap.putBlock(NekomaTweaks.REDSTONE_CABLE_BLOCK, BlockRenderLayer.CUTOUT)
-//        BlockRenderLayerMap.putBlock(NekomaTweaks.REDSTONE_CABLE_BLOCK, BlockRenderLayer.TRANSLUCENT)
+
+        // Register color provider for redstone cable to handle power level tinting
+        ColorProviderRegistry.BLOCK.register({ state, _, _, _ ->
+            RedstoneWireBlock.getWireColor(state.get(RedstoneWireBlock.POWER))
+        }, NekomaTweaks.REDSTONE_CABLE_BLOCK)
     }
 }
